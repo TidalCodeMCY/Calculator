8ac8dd188e0047cc9f2034116ec6ec3cf0d2df45
Mason Yates <Tidalabysstv@gmail.com>
2023-10-30 16:20:36 -0500

    finished calculator

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ class Calculator {
         if(x && y){
             return x+y;
         }else{
-            return 'Need two values';
+            return x;
         }
     }
 
@@ -21,7 +21,7 @@ class Calculator {
         if(x && y){
             return x-y;
         }else{
-            return 'Need two values';
+            return x;
         }
     }
 
@@ -29,11 +29,15 @@ class Calculator {
         if(x && y){
             return x*y;
         }else{
-            return 'Need two values';
+            return x;
         }
     }
 
     divide(x,y) {
+        if(x === 0 || y === 0){
+            return 'We dont divide by zero!'
+        }
+
         if(x && y){
             if(x > y){
                 return x/y;
@@ -41,7 +45,7 @@ class Calculator {
                 return y/x;
             }
         }else{
-            return 'Need two values';
+            return x;
         }
     }
 }
@@ -64,16 +68,30 @@ function calculate(target){
     }
 
     if(target.textContent === '/'||target.textContent === '*'||target.textContent === '+'||target.textContent === '-'){
+        if(newCalculator.method === '/' || newCalculator.method === '*' || newCalculator.method === '-' || newCalculator.method === '+'){
+            const foeTarget = document.createElement('div');
+            foeTarget.textContent = '=';
+            calculate(foeTarget);
+            newCalculator.setMethod(target.textContent);
+            calculatorscreen.textContent = calculatorscreen.textContent + target.textContent;
+            return;
+        }
         newCalculator.setMethod(target.textContent);
     }
 
     if(target.textContent === 'Clear'){
         calculatorscreen.textContent = '';
+        newCalculator.setMethod('');
         return;
     }
 
     if(target.textContent === '='){
         const arrg = calculatorscreen.textContent.split(newCalculator.method);
+
+        if(!arrg[1]){
+            return;
+        }
+
         if(newCalculator.method === '+'){
             const answer = newCalculator.add(parseInt(arrg[0]),parseInt(arrg[1]));
             calculatorscreen.textContent = answer;
@@ -85,6 +103,14 @@ function calculate(target){
             calculatorscreen.textContent = answer;
         }else if(newCalculator.method === '/') {
             let answer = newCalculator.divide(parseInt(arrg[0]),parseInt(arrg[1]));
+            if(typeof answer === 'string'){
+                const delay = 2000;
+                calculatorscreen.textContent = answer;
+                setTimeout(function() {
+                    calculatorscreen.textContent = arrg[0]+newCalculator.method;
+                },delay);
+                return;
+            }
             answer = parseFloat(answer.toFixed(3));
             calculatorscreen.textContent = answer;
         }
